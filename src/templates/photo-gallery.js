500bc2bdd0b6b4b5e9a9ed263027cd0b44b5fd4b
Christopher Shen <chrisshen@Christophers-MacBook-Pro.local>
2020-04-17 21:09:56 -0700

    gallery improvements

--- src/templates/photo-gallery.js
+++ src/templates/photo-gallery.js
@@ -1,30 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import SLayout from '../components/slayout';
-import Carousel from '../components/carousel';
 import SEO from '../components/seo';
+import Carousel, { Modal, ModalGateway } from 'react-images';
 import style from '../styles/photo-gallery.module.scss';
 
 const PhotoGalleryTemplate = ({ pageContext, location, data }) => {
-  const siteTitle = data.site.siteMetadata.title;
-  const imgs = data.allFile.edges.map(elem => {
-    return (<div>
-      <a href={elem.node.childImageSharp.original.src}>
-        <Img fluid={{ ...elem.node.childImageSharp.fluid, aspectRatio: 1}} className={style.thumbnail}/>
-      </a></div>);
-   }
-  );
+  const [modalVisibility, setModalVisibility] = useState(false);
+  const [index, setIndex] = useState(0);
 
-  const fluids = data.allFile.edges.map(elem =>
-    elem.node.childImageSharp.fluid
-  );
+  const siteTitle = data.site.siteMetadata.title;
+  const imgs = data.allFile.edges.map((elem, i) => {
+    return (<div onClick={() => {setIndex(i); setModalVisibility(true);}}>
+        <Img fluid={{ ...elem.node.childImageSharp.fluid, aspectRatio: 1}} className={style.thumbnail}/>
+      </div>);
+   });
+
+  const fullsize = data.allFile.edges.map(elem => {
+    return { source: elem.node.childImageSharp.original.src }
+  });
 
   return (
     <SLayout location={location.pathname} title={siteTitle}>
       <SEO title={pageContext.title} />
+      <ModalGateway>
+        {modalVisibility ? (
+          <Modal onClose={() => setModalVisibility(!modalVisibility)}>
+            <Carousel views={fullsize} currentIndex={index}/>
+          </Modal>
+        ) : null}
+      </ModalGateway>
       <div className={style.grid}>{ imgs }</div>
-      {/*<Carousel fluids={fluids} />*/}
     </SLayout>
   );
 }
